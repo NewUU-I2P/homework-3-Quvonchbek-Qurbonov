aab178bc4e02294507d6caeb56ba78e0e0c92d59
user <q.qurbonov@newuu.uz>
2023-11-10 22:34:44 +0500

    I edited programs

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,31 +2,15 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    cost=13.0f;
-    if (consumed_water>=0) {
-        if (consumed_water <= 30) {
-            cost = cost + consumed_water * 0.4f;
-            return cost;
-        } else {
-            cost = cost + 30 * 0.4f;
-            consumed_water = consumed_water - 30;
-        }
-        if (consumed_water <= 20) {
-            cost = cost + consumed_water * 0.12f;
-            return cost;
-        } else {
-            cost = cost + 20 * 0.12f;
-            consumed_water = consumed_water - 20;
-        }
-        if (consumed_water <= 10) {
-            cost = cost + consumed_water * 1.4f;
-            return cost;
-        } else {
-            cost = cost + 10 * 1.4f;
-            consumed_water = consumed_water - 10;
-        }
-        if (consumed_water > 0)
-            cost = cost + consumed_water * 1.5f;
-    }
+    cost=13;
+    if (cost<=30)
+        cost=cost+consumed_water*0.4;
+    else if(cost<=50 && cost>30)
+        cost=cost+30*0.4+(consumed_water-30)*0.12;
+    else if(cost>50 && cost<=60)
+        cost=cost+30*0.4+20*0.12+(consumed_water-30-20)*1.4;
+    else
+        cost=cost+30*0.4+20*0.12+10*1.4+(consumed_water-60)*1.5;
+
     return cost;
 }
